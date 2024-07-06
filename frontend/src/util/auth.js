4d8bf83c9cab5c2cc32854ccd0a704dd3cd82f91
Matilda-bit <polinaovras@gmail.com>
2024-07-07 01:10:17 +0300

    route protection

--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,8 +1,18 @@
-export function getAuthToken() {
-    const token = localStorage.getItem('token');
-    return token;
-  }
+import { redirect } from 'react-router-dom';
 
-  export function tokenLoader() {
-    return getAuthToken;
+export function getAuthToken() {
+  const token = localStorage.getItem('token');
+  return token;
+}
+
+export function tokenLoader() {
+  return getAuthToken();
+}
+
+export function checkAuthLoader() {
+  const token = getAuthToken();
+
+  if (!token) {
+    return redirect('/auth');
   }
+}
